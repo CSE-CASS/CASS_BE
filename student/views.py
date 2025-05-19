297e2e40ecd8a2cac6429c8eeae02dee7da62665
JAEHYUK LEE <107346845+jaehyuk2-lee@users.noreply.github.com>
2025-05-19 16:04:36 +0900

    feat: 매칭 신청 수락 거부, 문제 추가 기능 구현

--- student/views.py
+++ student/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
-from accounts.models import Student
+from accounts.models import Student, Teacher, CassUser
 from teacher.models import Problem, Matching
 from django.shortcuts import get_object_or_404
 
@@ -33,3 +33,43 @@ class StudentInfo(APIView):
             problems = []
 
         return Response({"isMatched": isMatched, "problems": problems})
+
+
+class MatchingRequest(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        student = get_object_or_404(Student, user=request.user)
+        teacher_id = request.data.get("teacher_id")
+
+        try:
+            teacher = Teacher.objects.get(user__username=teacher_id)
+        except Teacher.DoesNotExist:
+            return Response(
+                {
+                    "success": 0,
+                    "msg": "요청실패 - 존재하지않는 선생",
+                },
+                status=400,
+            )
+
+        if Matching.objects.filter(
+            teacher=teacher, student=student, status="pending"
+        ).exists():
+            return Response(
+                {
+                    "success": 0,
+                    "msg": "요청실패 - 수락 대기중",
+                },
+                status=400,
+            )
+
+        Matching.objects.create(teacher=teacher, student=student)
+        return Response(
+            {
+                "success": 1,
+                "msg": "요청성공",
+            },
+            status=201,
+        )
--- teacher/urls.py
+++ teacher/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("info", views.TeacherInfo.as_view()),
+    path("m_accept/<str:student_id>", views.MatchingAccept.as_view()),
+    path("m_reject/<str:student_id>", views.MatchingReject.as_view()),
+    path("add_problem", views.AddProblem.as_view()),
 ]
